9989b81a9ea99240e81b8afbd63eb3a2e4e7041c
Daniel Dias <danielsdias02@gmail.com>
2026-01-10 18:08:25 -0300

    Added more html routes for movimentacoes and categorias

--- webapp/blueprints/transacoes/routes.py
+++ webapp/blueprints/transacoes/routes.py
@@ -5,3 +5,13 @@ from flask import render_template
 @transacoes_bp.route("/adicionar_transacao", methods=["GET"])
 def adicionar_transacao():
     return render_template("adicionar_transacao.html")
+
+
+@transacoes_bp.route("/movimentacoes", methods=["GET"])
+def movimentacoes():
+    return render_template("movimentacoes.html")
+
+
+@transacoes_bp.route("/categorias", methods=["GET"])
+def categorias():
+    return render_template("categorias.html")
