45928e08497ea3dd85534452ed9189756325d400
Daniel Dias <danielsdias02@gmail.com>
2026-01-10 18:02:39 -0300

    Add inicial model for transactions on database

---
+++ webapp/models.py
@@ -0,0 +1,11 @@
+from . import db
+
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String(20), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    
